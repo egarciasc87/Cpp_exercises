74a6f5411f16f317a2670ad1453b2b9e248d11ce
Erick Garcia <egarciasc87@gmail.com>
2023-08-31 21:25:44 -0500

    Hello World

---
+++ 1_Basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello world..."<<std::endl;
+    std::cout<<"This is Erick, nice to meet you!"<<"\n";
+    std::cout<<"This is a new line.";
+    return 0;
+}
